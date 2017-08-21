8a6f20df8ea04e85a8cd4b1a39c14f77052ffe6a
cvk02 <31096004+cvk02@users.noreply.github.com>
2017-08-21 11:34:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article1',function(req, res)
+{
+    res.sendFile("Article one is requested >> ");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
