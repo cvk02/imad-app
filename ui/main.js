6c930a9d2e9d4cec2c0bcb59bbd24d339817bea4
cvk02 <31096004+cvk02@users.noreply.github.com>
2017-08-23 12:10:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 //console.log('Loaded!');
-var button = document.getElementById("magic");
+var button = document.getElementById("counter");
 
 var counter = 0;
 button.onclick = function(req,res)
 {
     
     counter = counter +1;
-    var span = document.getElementById("magic");
+    var span = document.getElementById("counter");
     span.innerHTML = counter.toString();
 }
